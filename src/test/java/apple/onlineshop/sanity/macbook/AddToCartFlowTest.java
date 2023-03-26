3bdf2696260fd4ee130dff532485f5aa0ede1efb
gitstanhub <stasdmitruk1@gmail.com>
2023-03-26 17:54:22 +0200

    Added propertie files drafts, moved AddToCartFlowTest dependencies into a separate class

--- src/test/java/apple/onlineshop/sanity/macbook/AddToCartFlowTest.java
+++ src/test/java/apple/onlineshop/sanity/macbook/AddToCartFlowTest.java
@@ -1,36 +1,9 @@
 package apple.onlineshop.sanity.macbook;
 
-import TestData.MBPModelsData;
-import elements.BundleSelector;
-import elements.ConfigSummaryBar;
-import elements.NavBar;
-import elements.SummaryHeader;
-import locators.CategoryLocators;
-import locators.MBPConfigLocators;
-import locators.MBPLocators;
+import base.CartTestBase;
 import org.junit.jupiter.api.Test;
-import pages.HomePage;
-import pages.mac.MBPCommonLandingPage;
-import pages.mac.buy.model.MBP16BuyPage;
 
-public class AddToCartFlowTest {
-
-    HomePage homePage = new HomePage();
-    NavBar navBar = new NavBar();
-    MBPCommonLandingPage mbpCommonLandingPage = new MBPCommonLandingPage();
-    BundleSelector bundleSelector = new BundleSelector();
-    MBP16BuyPage mbp16BuyPage = new MBP16BuyPage();
-    ConfigSummaryBar configSummaryBar = new ConfigSummaryBar();
-
-    CategoryLocators categoryLocators = new CategoryLocators();
-
-    MBPLocators mbpLocators = new MBPLocators();
-
-    MBPConfigLocators mbpConfigLocators = new MBPConfigLocators();
-
-    SummaryHeader summaryHeader = new SummaryHeader();
-
-    MBPModelsData mbpModelsData = new MBPModelsData();
+public class AddToCartFlowTest extends CartTestBase {
 
     @Test
     public void addMacBookPro() {
